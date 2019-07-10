c3d10bec1373faaf9154cb77f550d58c1ae1d18d
Felix Yin <lafenglafenghaha@163.com>
2019-07-10 15:55:48 +0800

    1.修改BarChartsViewController进行测试

--- ChartsDemo-iOS/Objective-C/Demos/BarChartViewController.m
+++ ChartsDemo-iOS/Objective-C/Demos/BarChartViewController.m
@@ -20,6 +20,7 @@
 @property (nonatomic, strong) IBOutlet UISlider *sliderY;
 @property (nonatomic, strong) IBOutlet UITextField *sliderTextX;
 @property (nonatomic, strong) IBOutlet UITextField *sliderTextY;
+@property(nonatomic,assign) NSInteger count;
 
 @end
 
@@ -28,7 +29,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    self.count = 7;
     self.title = @"Bar Chart";
     
     self.options = @[
@@ -50,7 +51,7 @@
     _chartView.delegate = self;
     
     _chartView.drawBarShadowEnabled = NO;
-    _chartView.drawValueAboveBarEnabled = YES;
+    _chartView.drawValueAboveBarEnabled = NO;
     
     _chartView.maxVisibleCount = 60;
     
@@ -59,7 +60,7 @@
     xAxis.labelFont = [UIFont systemFontOfSize:10.f];
     xAxis.drawGridLinesEnabled = NO;
     xAxis.granularity = 1.0; // only intervals of 1 day
-    xAxis.labelCount = 7;
+    xAxis.labelCount = self.count;
     xAxis.valueFormatter = [[DayAxisValueFormatter alloc] initForChart:_chartView];
     
     NSNumberFormatter *leftAxisFormatter = [[NSNumberFormatter alloc] init];
@@ -76,34 +77,37 @@
     leftAxis.spaceTop = 0.15;
     leftAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
     
-    ChartYAxis *rightAxis = _chartView.rightAxis;
-    rightAxis.enabled = YES;
-    rightAxis.drawGridLinesEnabled = NO;
-    rightAxis.labelFont = [UIFont systemFontOfSize:10.f];
-    rightAxis.labelCount = 8;
-    rightAxis.valueFormatter = leftAxis.valueFormatter;
-    rightAxis.spaceTop = 0.15;
-    rightAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
+    _chartView.leftAxis.enabled = NO;
+    _chartView.rightAxis.enabled = NO;
+//    ChartYAxis *rightAxis = _chartView.rightAxis;
+//    rightAxis.enabled = YES;
+//    rightAxis.drawGridLinesEnabled = NO;
+//    rightAxis.labelFont = [UIFont systemFontOfSize:10.f];
+//    rightAxis.labelCount = 8;
+//    rightAxis.valueFormatter = leftAxis.valueFormatter;
+//    rightAxis.spaceTop = 0.15;
+//    rightAxis.axisMinimum = 0.0; // this replaces startAtZero = YES
     
     ChartLegend *l = _chartView.legend;
-    l.horizontalAlignment = ChartLegendHorizontalAlignmentLeft;
-    l.verticalAlignment = ChartLegendVerticalAlignmentBottom;
-    l.orientation = ChartLegendOrientationHorizontal;
-    l.drawInside = NO;
-    l.form = ChartLegendFormSquare;
-    l.formSize = 9.0;
-    l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:11.f];
-    l.xEntrySpace = 4.0;
+    l.enabled = NO;
+//    l.horizontalAlignment = ChartLegendHorizontalAlignmentLeft;
+//    l.verticalAlignment = ChartLegendVerticalAlignmentBottom;
+//    l.orientation = ChartLegendOrientationHorizontal;
+//    l.drawInside = NO;
+//    l.form = ChartLegendFormSquare;
+//    l.formSize = 9.0;
+//    l.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:11.f];
+//    l.xEntrySpace = 4.0;
     
-    XYMarkerView *marker = [[XYMarkerView alloc]
-                                  initWithColor: [UIColor colorWithWhite:180/255. alpha:1.0]
-                                  font: [UIFont systemFontOfSize:12.0]
-                                  textColor: UIColor.whiteColor
-                                  insets: UIEdgeInsetsMake(8.0, 8.0, 20.0, 8.0)
-                                  xAxisValueFormatter: _chartView.xAxis.valueFormatter];
-    marker.chartView = _chartView;
-    marker.minimumSize = CGSizeMake(80.f, 40.f);
-    _chartView.marker = marker;
+//    XYMarkerView *marker = [[XYMarkerView alloc]
+//                                  initWithColor: [UIColor colorWithWhite:180/255. alpha:1.0]
+//                                  font: [UIFont systemFontOfSize:12.0]
+//                                  textColor: UIColor.whiteColor
+//                                  insets: UIEdgeInsetsMake(8.0, 8.0, 20.0, 8.0)
+//                                  xAxisValueFormatter: _chartView.xAxis.valueFormatter];
+//    marker.chartView = _chartView;
+//    marker.minimumSize = CGSizeMake(80.f, 40.f);
+//    _chartView.marker = marker;
     
     _sliderX.value = 12.0;
     _sliderY.value = 50.0;
@@ -130,17 +134,35 @@
 - (void)setDataCount:(int)count range:(double)range
 {
     double start = 1.0;
-    
+    count = self.count;
     NSMutableArray *yVals = [[NSMutableArray alloc] init];
     
-    for (int i = start; i < start + count + 1; i++)
+    NSInteger spacePercent = 2;
+    NSInteger barWidthPercent = 1;//0.85;
+    
+    CGFloat barWidth = 37;
+    CGFloat space = 0;//(spacePercent - barWidthPercent)*37;
+    CGFloat totalWidth = CGRectGetWidth(self.chartView.frame);
+    NSInteger visibleCount = (totalWidth - space)/(barWidth+space);
+    NSLog(@"%@",@((totalWidth - space)/(barWidth+space)));
+    CGFloat averageWidth = (totalWidth/(visibleCount*2-1));
+    CGFloat scale = barWidth/averageWidth;
+    if (visibleCount > count) {
+        scale = barWidth/((totalWidth - 20)/(count*2-1));
+    }
+    NSLog(@"averageWidth == %@  totalWidth == %@ scale == %@",@(averageWidth),@(totalWidth),@(scale));
+    
+    for (int i = start; i < start + count; i++)
     {
         double mult = (range + 1);
         double val = (double) (arc4random_uniform(mult));
+        if (val < 1) {
+            val = 10;
+        }
         if (arc4random_uniform(100) < 25) {
-            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i y:val icon: [UIImage imageNamed:@"icon"]]];
+            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i*spacePercent y:val icon: [UIImage imageNamed:@"icon"]]];
         } else {
-            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i y:val]];
+            [yVals addObject:[[BarChartDataEntry alloc] initWithX:i*spacePercent y:val]];
         }
     }
     
@@ -160,6 +182,7 @@
         //        set1.highlightColor = [UIColor redColor]; //单色
         set1.highlightGradientColors = @[@[[UIColor redColor],[UIColor blackColor]]]; //高亮渐变色
         set1.drawIconsEnabled = NO;
+        set1.drawValuesEnabled = NO;
         set1.barNeedRedius = YES;
         
         NSMutableArray *dataSets = [[NSMutableArray alloc] init];
@@ -167,10 +190,17 @@
         
         BarChartData *data = [[BarChartData alloc] initWithDataSets:dataSets];
         [data setValueFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:10.f]];
-        
-        data.barWidth = 0.9f;
+        _chartView.fitBars = NO;
+        data.barWidth = barWidthPercent;
         
         _chartView.data = data;
+        
+        [_chartView setVisibleXRangeMaximum:visibleCount];
+        if (scale < 1) {
+            [_chartView.viewPortHandler setMaximumScaleX:scale];
+        }else{
+            //            [_chartView.viewPortHandler setMinimumScaleX:scale];
+        }
     }
 }
 
